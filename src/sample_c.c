2f029b1a876b5b6b6f750365b22aa560b6bb0716
conand <andreacontinella@gmail.com>
2015-11-08 20:23:58 +0100

    add menu

--- src/sample_c.c
+++ src/sample_c.c
@@ -8,9 +8,10 @@
 #define MAX_VALUE 1000
 
 
-float read_float(){
+float read_float(void){
     char input[16];
-    read(0, input, 15);
+    int size;
+    size = read(0, input, 15);
     input[15] = '\0'; 
 
     return (float) atof(input); 
@@ -56,12 +57,22 @@ void welcome(void){
     fflush(stdout);
 }
 
+void print_menu(void){
+    printf("\n1) Make an offer\n");
+    printf("2) Quit\n");
+    printf("?: ");
+    fflush(stdout);
+}
+
 int main(){
     float last_bets[N];
     float player_bet;
     float user_bet;
     int16_t counter;
+    int8_t keep_playing;
     time_t start_time;
+    char choice;
+    int8_t i;
 
     srand(time(NULL));
     memset(last_bets, 0, sizeof(float) * N);
@@ -70,12 +81,33 @@ int main(){
     start_time = time(NULL);
 
     user_bet = 100 + (rand() % 50);
+    keep_playing = 1;
 
-   
-    while(time(NULL) - start_time < TIME_WINDOW){
-        play(&player_bet, &user_bet);
-        last_bets[counter % N] = player_bet;
-        counter++;
+    while(time(NULL) - start_time < TIME_WINDOW && keep_playing){
+        print_menu();
+        choice = getchar();
+        fflush(stdin);
+
+        switch (choice) {
+            case '1':
+                play(&player_bet, &user_bet);
+                last_bets[counter % N] = player_bet;
+                counter++;
+                break;
+            case '2':
+                keep_playing = 0;
+                counter--;
+                break;
+            default:
+                break;
+        }
+    }
+
+    printf("Last bets:\n");
+    while (i < N && last_bets[counter % N]){
+        printf("- %.2f\n", last_bets[counter % N]);
+        counter--;
+        i++;
     }
 
     return 0;
