25de353e3cf2b88feaae064939451c1303b3c004
conand <andreacontinella@gmail.com>
2015-11-24 00:40:16 +0100

    add new/admin auction functions

--- src/sample_c.c
+++ src/sample_c.c
@@ -2,42 +2,64 @@
 #include <stdint.h>
 #include <time.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define N 10
+#define N 15
 #define TIME_WINDOW 120
-#define MAX_VALUE 1500
+#define MAX_VALUE 150000
 #define KEEP_PLAYING 1
 #define STOP_PLAYING 0
 #define NEW_OFFER_MAX_PLUS 10
 #define STOP_PLAYING 0
+#define AUCTION_ID_SIZE 20
+#define PASSWORD_SIZE 20
+#define IMEI_SIZE 50
 
 
-float read_float(void){
+double read_double(void){
     char input[16];
     int size;
 
     size = read(0, input, 15);
     input[15] = '\0'; 
 
-    return (float) atof(input); 
+    return (double) atof(input); 
 }
 
+void rand_string(char *str, size_t size){
+    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    size_t n;
+
+    if (size) {
+        --size;
+        for (n = 0; n < size; n++) {
+            int key = rand() % (int) (sizeof(charset) - 1);
+            str[n] = charset[key];
+        }
+        str[size] = '\0';
+    }
+}
 
 int make_new_offer(int player_bet){
-    float tmp = rand() % (MAX_VALUE * 2 / 3) + MAX_VALUE / 3;
+    double tmp = rand() % (MAX_VALUE * 2 / 3) + MAX_VALUE / 3;
 
     return tmp > player_bet;
 }
 
 
-int play(float *player_bet, float *user_bet){
+int play(double *player_bet, double *user_bet, double *last_bets, int8_t *counter){
 
     do{
         printf("Best offer by now is %.2f$. You have to offer more!\n", *user_bet);
         printf("How much do you want to offer?\n");
         fflush(stdout);
     
-        *player_bet = read_float();
+        *player_bet = read_double();
+        
+        (*counter)++;
+        last_bets[*counter % N] = *player_bet;
+        printf("%d - %p\n", *counter, last_bets + (*counter % N));
+        fflush(stdout);
 
     } while(*player_bet <= *user_bet);
 
@@ -89,21 +111,27 @@ void print_menu(void){
     fflush(stdout);
 }
 
-int main(){
-    float last_bets[N];
-    float player_bet;
-    float user_bet;
-    int16_t counter;
+void participate(){
+    double last_bets[N];
+    double player_bet;
+    double user_bet;
+    int8_t x = 0;
     int8_t keep_playing;
     time_t start_time;
-    char choice;
+    char choice[128];
     int8_t i;
+    int8_t counter;
 
-    srand(time(NULL));
-    memset(last_bets, 0, sizeof(float) * N);
+    memset(last_bets, 0, sizeof(double) * N);
+    memset(choice, 0, sizeof(char) * 128);
     
     welcome();
     start_time = time(NULL);
+
+    printf("x %p\n", &x);
+    printf("last_bets %p\n", last_bets);
+    printf("player_bet %p\n", &player_bet);
+    printf("counter %p\n", &counter);
 
     user_bet = 100 + (rand() % 50);
     keep_playing = 1;
@@ -111,14 +139,12 @@ int main(){
     counter = 0;
     while(time(NULL) - start_time < TIME_WINDOW && keep_playing){
         print_menu();
-        choice = getchar();
+        read(0, choice, 128);
         fflush(stdin);
 
-        switch (choice) {
+        switch (choice[0]) {
             case '1':
-                keep_playing = play(&player_bet, &user_bet);
-                counter++;
-                last_bets[counter % N] = player_bet;
+                keep_playing = play(&player_bet, &user_bet, last_bets, &counter);
                 break;
             case '2':
                 keep_playing = STOP_PLAYING;
@@ -141,7 +167,112 @@ int main(){
         counter--;
         i++;
     }
+    if (x)
+        printf("ASDASDASDASD");
     fflush(stdout);
     
     return 0;
+}
+
+void new_auction(){
+    char imei[IMEI_SIZE];
+    char passwd[PASSWORD_SIZE];
+    char auction_id[AUCTION_ID_SIZE];
+
+    memset(imei, 0, sizeof(char)*IMEI_SIZE);
+    memset(passwd, 0, sizeof(char)*PASSWORD_SIZE);
+
+    printf("Insert the IMEI of the (hano)iFon:\n");
+    fflush(stdout);
+    read(0, imei, IMEI_SIZE);
+    fflush(stdin);
+
+    rand_string(auction_id, AUCTION_ID_SIZE);
+    rand_string(passwd, PASSWORD_SIZE);
+
+    FILE *fp = fopen(auction_id, "w");
+    fprintf(fp, "%s\n%s\n", passwd, imei);
+    fclose(fp);
+
+    printf("Auction ID: %s\n", auction_id);
+    printf("Your Password: %s\n\n", passwd);
+
+}
+
+void admin_auction(){
+    char auction_id[AUCTION_ID_SIZE];
+    char passwd[PASSWORD_SIZE];
+    char correct_passwd[PASSWORD_SIZE];
+    char imei[IMEI_SIZE];
+    FILE *fp;
+
+    memset(auction_id, 0, sizeof(char)*AUCTION_ID_SIZE);
+    memset(passwd, 0, sizeof(char)*PASSWORD_SIZE);
+
+    printf("Insert the auction ID: ");
+    fflush(stdout);
+    read(0, auction_id, AUCTION_ID_SIZE);
+    auction_id[AUCTION_ID_SIZE-1] = '\0';
+    fflush(stdin);
+
+    if (fp = fopen(auction_id, "r")){        
+        fgets(correct_passwd, PASSWORD_SIZE, fp);
+    
+    } else {
+        printf("Auction does not exits\n");
+        fflush(stdout);
+        return;    
+    }
+
+    printf("Insert the password: ");
+    fflush(stdout);
+    read(0, passwd, PASSWORD_SIZE);
+    passwd[PASSWORD_SIZE-1] = '\0';
+    fflush(stdin);
+
+    if (!strncmp(passwd, correct_passwd, PASSWORD_SIZE)){
+        fgets(imei, IMEI_SIZE, fp);
+        fgets(imei, IMEI_SIZE, fp);
+        printf("Your IMEI: %s\n", imei);
+    }
+    else{
+        printf("Wrong password!\n");
+    }
+
+    fflush(stdout);
+    fclose(fp);
+    
+
+}
+
+int main(){
+    char choice[4];
+    
+    srand(time(NULL));
+
+    do{
+        printf("1) Create new auction\n");
+        printf("2) Admin your auction\n");
+        printf("3) Participate in an auction\n");
+        printf("4) Exit\n");
+        printf("?: \n");
+        fflush(stdout);
+
+        read(0, choice, 4);
+        fflush(stdin);
+        
+        switch (choice[0]){
+            case '1':
+                new_auction();
+                break;
+            case '2':
+                admin_auction();
+                break;
+            case '3':
+                participate();
+                break;
+            default:
+                break;
+        }
+    }while(choice[0] != '4');
 }
