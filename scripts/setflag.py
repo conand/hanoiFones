96247fe6d32d2ac880fa5cf3f129053f395747db
Marcello Pogliani <marcello.pogliani@polimi.it>
2015-11-25 12:17:03 +0100

    Tentative setflag.py script

--- scripts/setflag.py
+++ scripts/setflag.py
@@ -4,11 +4,28 @@ import socket
 import sys
 import pexpect
 import pexpect.fdpexpect
-
+import re
 
 def set_flag(ip, port, flag):
-    flag_id = "flag_id"
-    password = "password"
+
+    conn = socket.create_connection((ip,port))
+    c = pexpect.fdpexpect.fdspawn(conn.fileno())
+
+    c.expect('\?:')
+    print c.after
+    print c.sendline('1')
+    c.expect('Insert the IMEI of the \(hano\)iFon:')
+    print c.after
+    c.sendline(flag)
+    c.expect('Auction ID: \w+')
+    flag_id = re.search('^Auction ID: (\w+)$', c.after).group(1)
+    c.expect('Your Password: \w+')
+    password = re.search('^Your Password: (\w+)$', c.after).group(1)
+    c.expect('\?:')
+    c.sendline('4')    
+    c.close()
+    
+    conn.close()
 
     return {
             'FLAG_ID': flag_id,  # Unique id for each flag
@@ -17,4 +34,4 @@ def set_flag(ip, port, flag):
 
 
 if __name__ == "__main__":
-    print set_flag(None, None, "FLG_just_testing")
+    print set_flag('127.0.0.1', '4000', "FLG_just_testing")
